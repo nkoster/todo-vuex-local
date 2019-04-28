e866b46f0d8f3af4851c049acadce2ef74394eca
Niels Koster <n.koster@portavita.eu>
2019-04-29 01:17:19 +0200

    Cleanup.

--- app.js
+++ app.js
@@ -2,55 +2,26 @@
 const
   express = require('express'),
   { parse } = require('querystring'),
-  //bodyParser = require('body-parser'),
-  //cors = require('cors'),
   app = express(),
-  fs = require('fs'),
-  util = require('util')
+  log = false
 
-let db;
-
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
-
-// app.use(cors({
-//   'allowedHeaders': ['Content-Type'],
-//   'origin': '*',
-//   'preflightContinue': true
-// }))
+let db
 
 let allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
 }
-
-app.use(allowCrossDomain);
+app.use(allowCrossDomain)
 
 app.get('/api/v1/db', (req, res) => {
-  console.log(`GET ${req.route.path}`);
+  console.log(`GET ${req.route.path}`)
   res.header('Content-Type', 'application/json')
     .status(200).send(db)
 })
 
-function collectRequestData(request, callback) {
-  if(request.headers['content-type'] === 'application/x-www-form-urlencoded') {
-      let body = '';
-      request.on('data', chunk => {
-          body += chunk.toString();
-      });
-      request.on('end', () => {
-          callback(parse(body));
-      });
-  }
-  else {
-      callback(null);
-  }
-}
-
 app.post('/api/v1/db', (req, res) => {
-
   if (req.method === 'POST') {
     let body = '';
     req.on('data', chunk => {
@@ -58,11 +29,12 @@ app.post('/api/v1/db', (req, res) => {
     });
     req.on('end', () => {
         db = parse(body)
-        for (o in db) {
-          console.log(JSON.parse(o));
-          // break
+        if (log) {
+          for (o in db) {
+            console.log(JSON.parse(o));
+          }
         }
-        //console.log(db)
+        console.log('db updated')
         res.end();
     });
 }
