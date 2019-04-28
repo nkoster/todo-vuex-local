113bcb628af141f4669b69816e9cff4d47cb56d0
Niels Koster <n.koster@portavita.eu>
2019-04-28 23:30:37 +0200

    Stuff...

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+/* eslint-disable */
+
 const http = require('http')
 const { parse } = require('querystring')
-
-function log(msg) {
-    console.log((new Date()) + ' -- ' + msg)
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000,
+    'Content-Type': 'application/json; charset=utf-8'
 }
 
 function collectRequestData(request, callback) {
     if (request.headers['content-type'] === 'application/x-www-form-urlencoded') {
         let body = '';
         request.on('data', chunk => body += chunk.toString())
-        request.on('end', () => callback(parse(body)));
+        request.on('end', () => callback(parse(body)))
+        console.log(body)
     }
     else {
         callback(null)
@@ -17,27 +22,34 @@ function collectRequestData(request, callback) {
 }
 
 http.createServer({}, (req, res) => {
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204, headers)
+        res.end()
+        return
+    }    
     if (req.method === 'POST') {
+        console.log(req)
         collectRequestData(req, result => {
-            res.writeHeader(200, {'Content-Type': 'application/json; charset=utf-8'})
+            res.writeHead(200, headers)
             res.end(JSON.stringify(result))
         })
-    } else {
-        res.writeHeader(200, {'Content-Type': 'text/html; charset=utf-8'})
-        res.end(`
-        <!doctype html>
-        <html>
-        <body>
-            <form action="/" method="post">
-                <input type="text" name="fname" /><br />
-                <input type="number" name="age" /><br />
-                <input type="file" name="photo" /><br />
-                <button>Save</button>
-            </form>
-        </body>
-        </html>
-      `)
     }
+    //  else {
+    //     res.writeHeader(200, {'Content-Type': 'text/html; charset=utf-8'})
+    //     res.end(`
+    //     <!doctype html>
+    //     <html>
+    //     <body>
+    //         <form action="/" method="post">
+    //             <input type="text" name="fname" /><br />
+    //             <input type="number" name="age" /><br />
+    //             <input type="file" name="photo" /><br />
+    //             <button>Save</button>
+    //         </form>
+    //     </body>
+    //     </html>
+    //   `)
+    // }
 }).listen(5000, () => {
-    log('http server started')
+    console.log('http server started')
 })
