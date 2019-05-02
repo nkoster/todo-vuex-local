946052918986ff98f7939e2367e0220fd11290f7
Niels Koster <n.koster@portavita.eu>
2019-05-03 00:53:50 +0200

    Better parsing, and response, prepair for stateId

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const
   app = express(),
   log = true
 
-let db
+let db = {
+  stateId: 0,
+  todos: []
+}
 
 app.use((_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -15,8 +18,23 @@ app.use((_, res, next) => {
 })
 
 app.get('/api/v1/db', (_, res) => {
-  if (log) console.log('GET')
-  res.header('Content-Type', 'application/json').status(200).send(db)
+  if (log) {
+    console.log('GET')
+    if (log) {
+      for (let o in db) {
+        try {
+          console.log(JSON.parse(o))
+          db = JSON.parse(o)
+        }
+        catch(err) {
+          console.log(err)
+        }
+      }
+    }
+  }
+  if (db.todos) res.header('Content-Type', 'application/json').status(200).send({
+    todos: db.todos
+  })
 })
 
 app.post('/api/v1/db', (req, res) => {
@@ -27,9 +45,10 @@ app.post('/api/v1/db', (req, res) => {
         body += chunk.toString()
     })
     req.on('end', () => {
-        db = parse(body)
+        const read = parse(body)
+        if (read) db = read
         if (log) {
-          for (o in db) {
+          for (let o in db) {
             console.log(JSON.parse(o))
             console.log('db updated')
           }
